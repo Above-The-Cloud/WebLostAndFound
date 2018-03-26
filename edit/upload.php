1eeaa8d24b79fd8bc54ed36f7046c041fc84b7d0
yiwangchunyu <yiwangchunyu@163.com>
2018-03-27 00:41:53 +0800

    修复若干问题，功能完整

--- edit/upload.php
+++ edit/upload.php
@@ -5,7 +5,7 @@ echo json_encode($_FILES);
 echo json_encode($_POST);  
 $newname = " ";
 $publish_id = $_POST['publish_id'];
-
+$APPURL = 'https://yiwangchunyu.wang/';
 
 //echo json_encode($res);
 if ($_FILES["file"]["error"] > 0)
@@ -18,13 +18,13 @@ else
   $res = mysqli_query( $conn, $sql );
   $temp = explode(".", $_FILES["file"]["name"]);
   $extension = end($temp); 
-  move_uploaded_file($_FILES["file"]["tmp_name"], "image/". $_FILES["file"]["name"]);
-  $newname = "image/".date("Y").date("m").date("d").date("H").date("i").date("s").rand(100, 999).".".$extension;
-  rename("image/" . $_FILES["file"]["name"], $newname);
-
+  move_uploaded_file($_FILES["file"]["tmp_name"], "../image/".$_FILES["file"]["name"]);
+  $newname = date("Y").date("m").date("d").date("H").date("i").date("s").rand(100, 999).".".$extension;
+  rename("../image/".$_FILES["file"]["name"], "../image/".$newname);
+	$image_url = $APPURL."image/".$newname;
   $sql = "INSERT INTO image(publish_id, type, image_url, submission_time) ".
       "VALUES ".
-      "('$publish_id', '$extension', '$newname', current_time);";
+      "('$publish_id', '$extension', '$image_url', current_time);";
   $res = mysqli_query( $conn, $sql );
   echo json_encode($res);
 }
--- myinfo/show_user_publishing.php
+++ myinfo/show_user_publishing.php
@@ -5,9 +5,9 @@
 *
 */
 header("Content-type: text/html; charset=utf-8");
-include 'conn.php';
+include '../conn.php';
 $data = array();
-
+$user_id = $_GET["user_id"];
 class Publish 
 {
 public $publish_id;
@@ -15,13 +15,14 @@ public $user_id;
 public $nickName;
 public $avatarUrl;
 public $type;
+public $category;
 public $title;
 public $image_exist;
 public $submission_time;
 public $image_url = array();
 }
 
-$sql = "SELECT * FROM publish order by publish_id desc;";
+	$sql = "SELECT * FROM publish where user_id='$user_id' order by publish_id desc;";
 
 $res = mysqli_query( $conn, $sql );
 if (mysqli_num_rows($res) > 0) {
@@ -32,15 +33,20 @@ if (mysqli_num_rows($res) > 0) {
       $tmp_id =  $row["publish_id"];
       $publish->user_id = $row["user_id"];
       $publish->type = $row["type"];
+	  $publish->category = $row["category"];
       $publish->title = $row["title"];
       $publish->msg = $row["msg"];
       $publish->image_exist = $row["image_exist"];
       $publish->submission_time = $row["submission_time"];
 	  
-	  $sql_info = "select * from user_info where user_id = '$publish->publish_id';"
-		$res_info = mysqli_query( $conn, $sql_info );
-		$publish->nickName = $row["nickName"];
-		$publish->avatarUrl = $row["avatarUrl"];
+	  $sql_info = "select * from user_info where user_id = '$publish->user_id';";
+	  $res_info = mysqli_query( $conn, $sql_info );
+	  if (mysqli_num_rows($res) > 0) {
+		while($row = mysqli_fetch_assoc($res_info)){
+			$publish->nickName = $row["nickName"];
+			$publish->avatarUrl = $row["avatarUrl"];	
+		}
+	  }	
       $sql_image = "SELECT * FROM image WHERE publish_id= '$tmp_id';";
       $res_image = mysqli_query( $conn, $sql_image );
       if (mysqli_num_rows($res_image) > 0) {
