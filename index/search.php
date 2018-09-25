20d49310a31bd278fe7aced2ddf73a27ad40755a
yiwangchunyu <yiwangchunyu@163.com>
2018-09-25 16:21:15 +0800

    search

---
+++ index/search.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SpringRain
+ * Date: 2018/9/25
+ * Time: 16:06
+ */
+header("Content-type: text/html; charset=utf-8");
+include '../conn.php';
+$data = array();
+$key_word = $_GET['key'];
+
+class Publish
+{
+    public $publish_id;
+    public $user_id;
+    public $nickName;
+    public $avatarUrl;
+    public $type;
+    public $title;
+    public $image_exist;
+    public $submission_time;
+    public $image_url = array();
+}
+
+$sql = "SELECT * ".
+    "FROM publish ".
+    "where title like '%{$key_word}%' or msg like '%{$key_word}%' ".
+    "order by publish_id desc;";
+
+$res = mysqli_query( $conn, $sql );
+if (mysqli_num_rows($res) > 0) {
+    // 输出数据
+    while($row = mysqli_fetch_assoc($res)) {
+        $publish =new Publish();
+        $publish->publish_id = $row["publish_id"];
+        $tmp_id =  $row["publish_id"];
+        $publish->user_id = $row["user_id"];
+        $publish->type = $row["type"];
+        $publish->title = $row["title"];
+        $publish->msg = $row["msg"];
+        $publish->image_exist = $row["image_exist"];
+        $publish->submission_time = $row["submission_time"];
+
+
+        $sql_info = "select * from user_info where user_id = '$publish->user_id';";
+        $res_info = mysqli_query( $conn, $sql_info );
+        if (mysqli_num_rows($res) > 0) {
+            while($row = mysqli_fetch_assoc($res_info)){
+                $publish->nickName = $row["nickName"];
+                $publish->avatarUrl = $row["avatarUrl"];
+            }
+        }
+
+        $sql_image = "SELECT * FROM image WHERE publish_id= '$tmp_id';";
+        $res_image = mysqli_query( $conn, $sql_image );
+        if (mysqli_num_rows($res_image) > 0) {
+            while($row_image = mysqli_fetch_assoc($res_image)) {
+                $publish->image_url[] = $row_image["image_url"];
+            }
+        }
+        $data[]=$publish;
+
+    }
+}
+$json = json_encode($data);
+echo $json;
+mysqli_close($conn);
