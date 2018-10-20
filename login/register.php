706088c941ae3444f1f64596e5997014bc83f12e
yiwangchunyu <yiwangchunyu@163.com>
2018-10-20 23:39:03 +0800

    bugfix

--- login/register.php
+++ login/register.php
@@ -12,7 +12,7 @@ $stu_id = $_GET['user_id'];
 $stu_pass = $_GET['user_password'];
 
 $openid = $_GET['openid'];
-$nickName = $_GET['nickName'];
+//$nickName = $_GET['nickName'];
 $avatarUrl = $_GET['avatarUrl'];
 $tag=0;
 $url = 'http://202.120.82.2:8081/ClientWeb/pro/ajax/login.aspx';
@@ -32,19 +32,20 @@ if($code!=0){
 }
 if($res['ret']==1){
 	$tag="registered";
+    $nickName = $res['ret']['name'];
 	$sql_user_info = "select * from user_info where user_id = '$stu_id';";
 	$res_user_info = mysqli_query( $conn, $sql_user_info);
 	if(mysqli_num_rows($res_user_info) <=0){
 		$tag="unregistered";
 		$sql_insert_userinfo = "insert into user_info(user_id,nickName, avatarUrl, submission_time) VALUES ('$stu_id','$nickName', '$avatarUrl', current_time());";
 		$res = mysqli_query( $conn, $sql_insert_userinfo);
-		//if($res==$false){
+		if($res==false){
             lib\Response::send(-2, 'fail',['sql'=>$sql_insert_userinfo,'res'=>$res, 'error'=>mysqli_errno($conn)]);
-        //}
+        }
 	}
     $sql_insert_openid = "insert into user_openid(user_id,openid, submission_time) VALUES ('$stu_id','$openid', current_time());";
     $res = mysqli_query( $conn, $sql_insert_openid );
-    if($res==$false){
+    if($res==false){
         lib\Response::send(-2, 'fail',['sql'=>$sql_insert_openid,'error'=>mysqli_errno($conn)]);
     }
 	//echo json_encode($tag);
