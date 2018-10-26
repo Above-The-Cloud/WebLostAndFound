73cffbb20e38277c8ba52738fe388eefa71662fd
yiwangchunyu <yiwangchunyu@163.com>
2018-10-27 00:27:47 +0800

    换新appid

--- getopenid.php
+++ getopenid.php
@@ -1,8 +1,8 @@
 <?php
 	$data = $_GET;
 	//echo json_encode($data);
-	$appid="wxd4a02c9d70b821e8";
-	$appsecret="5c67cffb86a2e9d07f75d13b79218200";
+	$appid="wxf6a3fba14703a990";
+	$appsecret="6df255640af2d914b26f2e75f405d9a7";
 	$code=$_GET['code'];
 	$get_url="https://api.weixin.qq.com/sns/jscode2session?appid=".$appid."&secret=".$appsecret."&js_code=".$code."&grant_type=authorization_code";
 	$get_return = file_get_contents($get_url);
