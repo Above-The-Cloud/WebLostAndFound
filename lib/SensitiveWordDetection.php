da4461ae47a073a2d76a0063b5c0460d80e5de40
yiwangchunyu <yiwangchunyu@163.com>
2018-11-26 23:39:52 +0800

    WSD

---
+++ lib/SensitiveWordDetection.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SpringRain
+ * Date: 2018/11/26
+ * Time: 20:25
+ */
+
+namespace lib;
+
+require_once "dep.php";
+
+class SensitiveWordDetection
+{
+
+    private $log;
+    /**
+     * SensitiveWordDetection constructor.
+     */
+    public function __construct()
+    {
+        $this->log = new \lib\Log();
+    }
+
+    public static function send($querys){
+        $host = "http://apistore.tongchengyue.com";
+        $path = "/sw/isContains";
+        $method = "POST";
+        $appcode = "f560138f1b514ca98e7cf8ca12a23d85";
+        $headers = array();
+        array_push($headers, "Authorization:APPCODE " . $appcode);
+        //根据API的要求，定义相对应的Content-Type
+        array_push($headers, "Content-Type".":"."application/x-www-form-urlencoded; charset=UTF-8");
+//        $querys = "";
+        $bodys = "src=".$querys;
+        $url = $host . $path;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        if (1 == strpos("$".$host, "https://"))
+        {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $bodys);
+        $res = curl_exec($curl);
+        (new \lib\Log())->debug($res);
+        return $res;
+    }
+}
---
+++ lib/dep.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SpringRain
+ * Date: 2018/11/26
+ * Time: 22:41
+ */
+require_once "Log.php";
+require_once "RR.php";
