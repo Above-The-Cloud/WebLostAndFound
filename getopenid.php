8933a62c90270658af93068e711c0fcc87619899
yiwangchunyu <yiwangchunyu@163.com>
2019-04-19 22:57:14 +0800

    openid

--- getopenid.php
+++ getopenid.php
@@ -2,7 +2,7 @@
 	$data = $_GET;
 	//echo json_encode($data);
 	$appid="wxd8d5a2f6fa7f1878";
-	$appsecret="064157612113b2490b1941c7b0e77fda";
+	$appsecret="001gjwq00HrkqH14SDs00PXMq00gjwq4";
 	$code=$_GET['code'];
 	$get_url="https://api.weixin.qq.com/sns/jscode2session?appid=".$appid."&secret=".$appsecret."&js_code=".$code."&grant_type=authorization_code";
 	$get_return = file_get_contents($get_url);
